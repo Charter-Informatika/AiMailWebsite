3f09e05b576cbdd98aca1d15864b7daf5ab1a735
KistDeovCharter <kistuttosi.david@charter.hu>
2026-01-26 11:22:52 +0100

    1.2.4

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -2,7 +2,7 @@ import { createReadStream } from "fs";
 import path from "path";
 
 export async function GET(req: Request) {
-  const fileUrl = "https://github.com/KistDeov/AiServiceApp/releases/download/1.2.2/aimail-Setup-1.2.2.exe";
+  const fileUrl = "https://github.com/Charter-Informatika/AiMail/releases/download/1.2.4/aimail-Setup-1.2.4.exe";
   return new Response(null, {
     status: 302,
     headers: {

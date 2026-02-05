ad0e6cd0c634b57d1e56b6a9c3fe0d376002fa34
KistDeovCharter <kistuttosi.david@charter.hu>
2026-02-05 08:17:14 +0100

    ascii -> unicode

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -4,12 +4,14 @@ export function emailValidation(value: string) {
 
 /**
  * Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character
+ * Supports Unicode characters (including accented letters like Á, É, Ő, Ű, etc.)
  */
 export function passwordValidation(val: string) {
-  return (
-    /[A-Z]/.test(val) &&
-    /[a-z]/.test(val) &&
-    /\d/.test(val) &&
-    /[@$!%*?&]/.test(val)
-  );
+  
+  const hasUppercase = /\p{Lu}/u.test(val);
+  const hasLowercase = /\p{Ll}/u.test(val);
+  const hasDigit = /\d/.test(val);
+  const hasSpecial = /[^\p{L}\p{N}\s]/u.test(val);
+  
+  return hasUppercase && hasLowercase && hasDigit && hasSpecial;
 }
